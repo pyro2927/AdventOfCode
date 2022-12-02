e4cab8838f277444a8ef3285ba4a9333e8f3738b
Joseph Pintozzi <joseph@pintozzi.com>
2022-12-01 20:11:42 -0600

    Day 1, Star 2

--- 2022/01/parse.py
+++ 2022/01/parse.py
@@ -1,12 +1,17 @@
 m = 0
 local = 0
 
+elves = []
+
 with open('input.txt', 'r') as file:
   for line in file:
     if len(line.strip()) == 0:
-      m = max(m, local)
+      elves.append(local)
       local = 0
     else:
       local += int(line.strip())
-    
-print(m)
+
+elves.sort(reverse=True)
+
+print("Elf 1: {}".format(elves[0]))
+print("Elfs 1-3: {}".format(sum(elves[0:3])))
