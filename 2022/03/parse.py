91eca878d073741dde3365b5d801db263c571c5e
Joseph Pintozzi <joseph@pintozzi.com>
2022-12-02 23:47:45 -0600

    Day 3, Star 2

--- 2022/03/parse.py
+++ 2022/03/parse.py
@@ -3,9 +3,26 @@ import string
 total = 0
 letters = list(string.ascii_letters)
 
+triplet = 0
+badges = 0
+# Potential badges for this group
+potentials = letters
+
 with open('input.txt', 'r') as file:
   for line in file:
     line = line.strip()
+    # narrow down potential badges
+    potentials = list(set(line) & set(potentials))
+    triplet += 1
+
+    if triplet == 3:
+      if len(potentials) > 1:
+        print("We have a problem")
+      elif len(potentials) == 1:
+        badges += letters.index(potentials[0]) + 1
+        potentials = letters
+        triplet = 0
+
     half = int(len(line)/2)
     first = line[0:half]
     second = line[half:len(line)]
@@ -13,4 +30,5 @@ with open('input.txt', 'r') as file:
     for letter in in_both:
       total += (letters.index(letter) + 1)
 
-print(total)
+print("Priority 1: " + str(total))
+print("Badges 2: " + str(badges))
