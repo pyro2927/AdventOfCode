3b54f30e9156ca38669b673b8ea4c6c9f5a7c610
Joseph Pintozzi <joseph@pintozzi.com>
2022-12-08 23:48:56 -0600

    Day 9 Star 2

--- 2022/09/parse.py
+++ 2022/09/parse.py
@@ -1,45 +1,19 @@
 spots = []
-h = [0,0]
-t = [0,0]
+rope = []
 
-# Won't work for real data due to range
-def debug():
-  for x in reversed(range(5)):
-    for y in range(6):
-      if h == [x,y]:
-        print("H", end="")
-      elif t == [x,y]:
-        print("T", end="")
-      elif (x,y) in spots:
-        print("#", end="")
-      else:
-        print(".", end="")
-    print("")
-  print("")
+for _ in range(10):
+  rope.append([0,0])
 
-
-def move_head(direction):
-  global h
-  global t
-
-  match direction:
-    case "R":
-      h[1] += 1
-    case "L":
-      h[1] -= 1
-    case "U":
-      h[0] += 1
-    case "D":
-      h[0] -= 1
-
+# Never call on head
+def determine_move(knot):
+  h = rope[knot - 1]
+  t = rope[knot]
   # determine if we need to move diagonal
   move_diag = (abs(h[0] - t[0]) + abs(h[1] - t[1])) > 2
   if move_diag:
     offset = 0
   else:
     offset = 1
-
-
   # make the tail follow
   if t[0] < h[0] - offset:
     t[0] += 1
@@ -51,9 +25,23 @@ def move_head(direction):
   elif t[1] > h[1] + offset:
     t[1] -= 1
 
-  spots.append((t[0], t[1]))
-  # Uncomment to see each step
-  #debug()
+def move_head(direction):
+  head = rope[0]
+  match direction:
+    case "R":
+      head[1] += 1
+    case "L":
+      head[1] -= 1
+    case "U":
+      head[0] += 1
+    case "D":
+      head[0] -= 1
+
+  for i in range(1,10):
+    determine_move(i)
+
+  tail = rope[9]
+  spots.append((tail[0], tail[1]))
     
 
 with open('input.txt', 'r') as file:
@@ -62,6 +50,5 @@ with open('input.txt', 'r') as file:
     for _ in range(int(count)):
       move_head(direction)
 
-#debug()
 # Make this a set to uniq
 print(len(set(spots)))
