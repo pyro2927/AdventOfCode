b5f48367c9213caa3f12e2ff3ee2f13db0c5cb03
Joseph Pintozzi <joseph@pintozzi.com>
2025-12-02 10:26:42 -0600

    Day 2 part 2

--- 2025/02/parse.py
+++ 2025/02/parse.py
@@ -7,8 +7,12 @@ with open(file_path, 'r') as file:
       first, last = idset.split('-')
       for product in range(int(first), int(last) + 1): # range is exclusive of stop
         product_string = str(product)
-        half = int(len(product_string) / 2)
-        if product_string[0:half] == product_string[half:]:
-          password += product
-
+        for chunksize in range(1, int(len(product_string) / 2) + 1):
+          # if it can't be evenly split, skip
+          if len(product_string) % chunksize != 0:
+            continue
+          chunks = set([product_string[i:i + chunksize] for i in range(0, len(product_string), chunksize)]) 
+          if len(chunks) == 1: # set() automatically strips duplicates
+            password += product
+            break
 print(password)
