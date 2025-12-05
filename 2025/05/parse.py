d6d66eb4bc1751bc16972af5768d9eafd3458656
Joseph Pintozzi <joseph@pintozzi.com>
2025-12-05 09:39:39 -0600

    Day 5 part 1

--- 2025/05/parse.py
+++ 2025/05/parse.py
@@ -1,16 +1,19 @@
 file_path = "input.txt"
 
 password = 0
-fresh = set()
+ranges = []
 
 with open(file_path, 'r') as file:
   for line in file:
     stripped = line.strip()
     if '-' in stripped:
       first, last = map(int, stripped.split('-'))
-      fresh.update(list(range(first, last + 1)))
+      ranges.append((first, last))
     elif len(stripped) > 0:
-      if int(stripped) in fresh:
-        password += 1
+      for r in ranges:
+        i = int(stripped)
+        if r[0] <= i and i <= r[1]:
+          password += 1
+          break
 
 print(password)
