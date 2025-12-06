41d520e6bb20113d8f0fd5bf1e07d80382a7a3e0
Joseph Pintozzi <joseph@pintozzi.com>
2025-12-06 08:55:48 -0600

    Day 5

--- 2025/05/parse.py
+++ 2025/05/parse.py
@@ -3,6 +3,8 @@ file_path = "input.txt"
 password = 0
 ranges = []
 
+# part 1
+
 with open(file_path, 'r') as file:
   for line in file:
     stripped = line.strip()
@@ -17,3 +19,24 @@ with open(file_path, 'r') as file:
           break
 
 print(password)
+
+# part 2
+sorted_ranges = sorted(ranges, key=lambda x:x[0])
+
+compressed_ranges = []
+compressed_ranges.append(sorted_ranges.pop(0))
+
+# compress
+for r in sorted_ranges:
+  last = compressed_ranges[-1]
+  if r[0] <= last[1]: # see if we're within
+    compressed_ranges[-1] = (last[0], max(r[1], last[1]))
+  else:
+    compressed_ranges.append(r)
+      
+fresh = len(compressed_ranges)
+
+for r in compressed_ranges:
+  fresh += (r[1] - r[0])
+
+print(fresh)
