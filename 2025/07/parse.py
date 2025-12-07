4cae6473eabdefad703fa095f0893331f01d3c0f
Joseph Pintozzi <joseph@pintozzi.com>
2025-12-07 14:36:51 -0600

    Day 7 part 2

--- 2025/07/parse.py
+++ 2025/07/parse.py
@@ -1,24 +1,31 @@
+from collections import defaultdict
+
 file_path = "input.txt"
 
 password = 0
 
-ts = set()
+ts = defaultdict(int)
 
 with open(file_path, 'r') as file:
   for line in file:
-    nt = set()
+    nt = defaultdict(int)
     c = list(line.strip())
     if len(ts) == 0:
-      ts.add(c.index('S'))
+      ts[c.index('S')] += 1
+      continue
 
-    for t in ts:
-      if c[t] == '^': # split
-        nt.add(t - 1)
-        nt.add(t + 1)
+    for k,v in ts.items():
+      if c[k] == '^': # split
+        nt[(k - 1)] += v
+        nt[(k + 1)] += v
         password += 1
       else:
-        nt.add(t)
-        
-    ts = nt
+        nt[k] += v
 
-print(password)
+    ts = nt
+    #print(f"{sum(ts.values()):03d} ", end='')
+    #print(''.join([str(nt[i] % 10) if i in nt else c[i] for i in range(0,15)])) # debug
+
+print("Part 1", password)
+
+print("Part 2", sum(ts.values()))
